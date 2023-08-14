51e2de592defbf4c72b3c26d56b2bad47bcd90c9
DarinelGuillen <darinel.escobar@outlook.com>
2023-08-14 16:27:28 -0600

    [FIX] Rutes´ve been fix all

---
+++ src/containers/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+// import Home from '../pages/Home'
+import Home from '../pages/Home.jsx'
+function App() {
+
+
+
+
+  return (
+    <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+function Home() {
+
+
+  return (
+    <>
+      <h1>HOLAAAAA</h1>
+    </>
+  );
+}
+export default Home;
