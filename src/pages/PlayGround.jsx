bf58f9db117ccb718b81474d7574e306f495bbc9
DarinelGuillen <darinel.escobar@outlook.com>
2023-08-15 12:47:39 -0600

    [ADD] web Component Input

--- src/pages/PlayGround.jsx
+++ src/pages/PlayGround.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import '../assets/css/MyInput.css';
+import '../assets/css/PlayGround.css';
 import MyInput from '../webComponents/MyInput.jsx';
 function Home() {
   const [inputValues, setInputValues] = useState({
@@ -17,12 +18,23 @@ function Home() {
 
   return (
     <>
-      <h1>This is an Example input</h1>
+    <div className="container">
+    <div className="box">
+      <h1>First Componets</h1>
+      <h1>Input simple look in console</h1>
+      <simple-input type="email" placeholder="Enter email" id="emailInput"  width="20%"></simple-input>
+      <simple-input type="password" placeholder="Enter password" id="passwordInput" width="50%"></simple-input>
+      <simple-input type="number" placeholder="Enter Phone" id="PhoneInput" width="90%"></simple-input>
+
+    </div>
+    <div className="box">
+    <h1>This is an Example input</h1>
       <my-test></my-test>
       <my-input label="Password:" placeholder="Your password here" width="100%" value="" id="password"></my-input>
       <my-input label="Mail:" placeholder="Your Mail here" width="190%" value=""></my-input>
-
-      <MyInput
+    </div>
+    <div className="box">
+    <MyInput
         label="Password:"
         placeholder="Your password here"
         width="100%"
@@ -38,9 +50,10 @@ function Home() {
         onChange={(value) => handleInputChange('email', value)}
         id="email"
       />
-      
-
-      <h1>TEST STEP CIRCLES </h1>
+    </div>
+    {/* !Steps circles */}
+    <div className='box'>
+    <h1>TEST STEP CIRCLES </h1>
       <ul
   data-te-stepper-init
   className="relative m-0 flex list-none justify-between overflow-hidden p-0 transition-[height] duration-200 ease-in-out">
@@ -116,6 +129,64 @@ function Home() {
     </div>
   </li>
 </ul>
+    </div>
+    <div className='box'>
+    
+            <div className="w-11/12 lg:w-2/6 mx-auto">
+                <div className="bg-gray-200 h-1 flex items-center justify-between">
+                    <div className="w-1/3 bg-indigo-700 h-1 flex items-center">
+                        <div className="bg-indigo-700 h-6 w-6 rounded-full shadow flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-check" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="#FFFFFF" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" />
+                                <path d="M5 12l5 5l10 -10" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="w-1/3 flex justify-between bg-indigo-700 h-1 items-center relative">
+                        <div className="absolute right-0 -mr-2">
+                            <div className="relative bg-white shadow-lg px-2 py-1 rounded mt-16 -mr-12">
+                                <svg className="absolute top-0 -mt-1 w-full right-0 left-0" width="16px" height="8px" viewBox="0 0 16 8" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                    <g id="Page-1" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
+                                        <g id="Progress-Bars" transform="translate(-322.000000, -198.000000)" fill="#FFFFFF">
+                                            <g id="Group-4" transform="translate(310.000000, 198.000000)">
+                                                <polygon id="Triangle" points="20 0 28 8 12 8" />
+                                            </g>
+                                        </g>
+                                    </g>
+                                </svg>
+                                <p className="text-indigo-700 text-xs font-bold">Step 3: Analyzing</p>
+                            </div>
+                        </div>
+                        <div className="bg-indigo-700 h-6 w-6 rounded-full shadow flex items-center justify-center -ml-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-check" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="#FFFFFF" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" />
+                                <path d="M5 12l5 5l10 -10" />
+                            </svg>
+                        </div>
+                        <div className="bg-white h-6 w-6 rounded-full shadow flex items-center justify-center -mr-3 relative">
+                            <div className="h-3 w-3 bg-indigo-700 rounded-full" />
+                        </div>
+                    </div>
+                    <div className="w-1/3 flex justify-end">
+                        <div className="bg-white h-6 w-6 rounded-full shadow" />
+                    </div>
+                </div>
+            </div> 
+
+    </div>
+    <div className='box'></div>
+    <div className='box'></div>
+    <div className='box'></div>
+    <div className='box'></div>
+    <div className='box'></div>
+    <div className='box'></div>
+  </div>
+      
+
+      
+      
+
+      
     </>
   );
 }
---
+++ src/webComponents/input.js
@@ -0,0 +1,50 @@
+class SimpleInput extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: 'open' });
+  
+      this.shadowRoot.innerHTML = `
+        <style>
+          input {
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            font-family: 'Open Sans', sans-serif;
+            color: linear-gradient(40deg, black 40%, transparent 40%);
+          }
+  
+          input::placeholder {
+            text-align: center;
+            font-family: 'Open Sans', sans-serif;
+            color: rgba(0, 0, 0, 0.4);  
+          }
+        </style>
+        <input type="text" placeholder="" />
+      `;
+  
+      this.inputElement = this.shadowRoot.querySelector('input');
+      this.inputElement.addEventListener('input', this.handleInputChange);
+    }
+  
+    connectedCallback() {
+      const placeholder = this.getAttribute('placeholder') || '';
+      const id = this.getAttribute('id') || '';
+      const width = this.getAttribute('width') || '100%';
+      const type = this.getAttribute('type') || 'text';
+  
+      this.inputElement.setAttribute('type', type);
+      this.inputElement.setAttribute('placeholder', placeholder);
+      this.inputElement.setAttribute('id', id);
+      this.inputElement.style.setProperty('width', width);
+    }
+  
+    handleInputChange = (e) => {
+      const newValue = e.target.value;
+      console.log("🚀 ~ file: input.js:44 ~ SimpleInput ~ e.target.value:", e.target.value)
+      this.dispatchEvent(new CustomEvent('inputChange', { detail: newValue }));
+    };
+  }
+  
+  customElements.define('simple-input', SimpleInput);
+  
