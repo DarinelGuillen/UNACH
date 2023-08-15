9b0aa4e304e0469d7e30492c79ce4147bccbb6a5
DarinelGuillen <darinel.escobar@outlook.com>
2023-08-14 18:24:18 -0600

    [ADD] Web Component´ve been add and it gets the current value, (Not Style just jet)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,54 @@
+import { useState } from 'react';
+import MyInput from '../webComponets/MyInput.jsx';
+import '../assets/css/MyInput.css';
+import '../assets/css/index.css';
 
 function Home() {
+  const [inputValues, setInputValues] = useState({
+    password: '',
+    email: '',
+    ExtraElementUser: '', // Add state for ExtraElementUser
+    // Add more properties for other inputs
+  });
 
+  const handleInputChange = (id, value) => {
+    setInputValues((prevValues) => ({
+      ...prevValues,
+      [id]: value,
+    }));
+    console.log("🚀 ~ file: Home.jsx:20 ~ handleInputChange ~ inputValues:", inputValues)
+  };
 
   return (
     <>
-      <h1>HOLAAAAA</h1>
+      <h1>This is an Example input</h1>
+      <MyInput
+        label="Password:"
+        placeholder="Your password here"
+        width="100%"
+        value={inputValues.password}
+        onChange={(value) => handleInputChange('password', value)}
+        id="password"
+      />
+      <MyInput
+        label="Mail:"
+        placeholder="Mail"
+        width="50%"
+        value={inputValues.email}
+        onChange={(value) => handleInputChange('email', value)}
+        id="email"
+      />
+      <MyInput
+        label="ExtraElementUser:"
+        placeholder="ExtraElementUser"
+        width="80%"
+        value={inputValues.ExtraElementUser}
+        onChange={(value) => handleInputChange('ExtraElementUser', value)}
+        id="ExtraElementUser"
+      />
+      {/* Add more MyInput components for other inputs */}
     </>
   );
 }
+
 export default Home;
