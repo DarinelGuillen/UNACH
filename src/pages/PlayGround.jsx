c3ffea88ba65b95d970cf782bbbebd2a4b8509b4
DarioAntonio <antoniodario10@hotmail.com>
2023-08-16 11:58:50 -0600

    [FIX] the pages and section 1 the form correctly

--- src/pages/PlayGround.jsx
+++ src/pages/PlayGround.jsx
@@ -37,23 +37,7 @@ function Home() {
             Código QR
           </div>
         </div>
-    </div>
-    <div className='grid'>
-      <div className='bg-white rounded-tl-lg grid'>
-        <div class="flex w-full justify-center items-center space-y-8">
-            <div class="w-full px-8 md:px-32 lg:px-24">
-                <form class="bg-white rounded-md shadow-2xl p-5">
-                
-                  <my-input placeholder="Título" width="100%" value=""></my-input>
-                  <my-input  placeholder="Fecha de elaboración de propuesta" width="100%" value=""></my-input>
-                  <my-input placeholder="Lugar de realización" width="100%" value=""></my-input>
-                
-                </form>
-            </div>
-        </div>
-      </div>
-    </div>
-    
+    </div>    
     <div className="container">
       {/* Input */}
     <div className="box">
