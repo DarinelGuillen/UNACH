9904c711ae52efc3e594a2586a1d8e65205b8de9
LuisAxelMarínGarcía <213467@ids.upchiapas.edu.mx>
2023-08-22 13:28:00 -0600

    iconos coloridos

--- src/atoms/ClaveDeAcceso.jsx
+++ src/atoms/ClaveDeAcceso.jsx
@@ -1,3 +1,5 @@
+import Iconos from './Iconos';
+
 function ClaveDeAcceso() {
     return (
         <>
@@ -12,31 +14,7 @@ function ClaveDeAcceso() {
                         <simple-input class="w-[200px] ml-[20px] sm:w-[250px] md:w-[300px] lg:w-[200px] lg:ml-[10px] xl:w-[300px]" height="20%" type="text" placeholder="DI" id="text"></simple-input>
                     </div>
                 </div>
-
-                <div className="flex w-full mt-5 justify-center">
-                    <ol class="flex items-center">
-                        <li class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-500">
-                            <span class="flex items-center text-white sm:md:text-xs md:text-sm justify-center w-20 h-20 bg-gray-100 rounded-full md:h-24 md:w-24 sm:w-20 sm:h-20 lg:h-24 lg:w-24 xl:h-28 xl:w-28 dark:bg-gray-500 shrink-0">
-                                <p className="text-sm xl:text-base">Enviando</p>
-                            </span>
-                        </li>
-                        <li class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-500">
-                            <span class="flex items-center text-white sm:md:text-xs md:text-sm justify-center w-20 h-20 bg-gray-100 rounded-full md:h-24 md:w-24 sm:w-20 sm:h-20 lg:h-24 lg:w-24 xl:h-28 xl:w-28 dark:bg-gray-500 shrink-0">
-                                <p className="text-sm xl:text-base">Validando</p>
-                            </span>
-                        </li>
-                        <li class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-500">
-                            <span class="flex items-center text-white sm:md:text-xs md:text-sm justify-center w-20 h-20 bg-gray-100 rounded-full md:h-24 md:w-24 sm:w-20 sm:h-20 lg:h-24 lg:w-24 xl:h-28 xl:w-28 dark:bg-gray-500 shrink-0">
-                                <p className="text-sm xl:text-base">Rectificando</p>
-                            </span>
-                        </li>
-                        <li class="flex items-center w-full">
-                            <span class="flex items-center text-white text-center sm:md:text-xs md:text-sm justify-center w-20 h-20 bg-gray-100 rounded-full md:h-24 md:w-24 sm:w-20 sm:h-20 lg:h-24 lg:w-24 xl:h-28 xl:w-28 dark:bg-gray-500 shrink-0">
-                                <p className="text-sm xl:text-base">Aprobado <br />o <br /> Rechazado</p>
-                            </span>
-                        </li>
-                    </ol>
-                </div>
+        <Iconos/>
             </div>
         </>
     );
---
+++ src/atoms/Iconos.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const ColorComponent = '0000'; // Cambia este valor según tus necesidades
+
+function Iconos() {
+    const obtenerClaseColor = (indice) => {
+        const valor = ColorComponent[indice];
+
+        if (valor === '0') {
+            return 'bg-gray-500';
+        } else if (valor === '1') {
+            return 'bg-green-500';
+        } else if (valor === '2') {
+            return 'bg-red-500';
+        }
+
+        return 'bg-gray-500'; // Valor incorrecto, se muestra en gris
+    };
+
+    return (
+        <>
+            <div className="flex w-full mt-5 justify-center">
+                <ol className="flex items-center">
+                    <li className={`flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-black-100 after:border-4 after:inline-block dark:after:border-black-500`}>
+                        <span className={`flex items-center text-white sm:md:text-xs md:text-sm justify-center w-20 h-20 ${obtenerClaseColor(0)} rounded-full md:h-24 md:w-24 sm:w-20 sm:h-20 lg:h-24 lg:w-24 xl:h-28 xl:w-28 dark:bg-gray-700 shrink-0`}>
+                            <p className="text-sm xl:text-base">Enviando</p>
+                        </span>
+                    </li>
+                    <li className={`flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-black-100 after:border-4 after:inline-block dark:after:border-black-500`}>
+                        <span className={`flex items-center text-white sm:md:text-xs md:text-sm justify-center w-20 h-20 ${obtenerClaseColor(1)} rounded-full md:h-24 md:w-24 sm:w-20 sm:h-20 lg:h-24 lg:w-24 xl:h-28 xl:w-28 dark:bg-gray-700 shrink-0`}>
+                            <p className="text-sm xl:text-base">Validando</p>
+                        </span>
+                    </li>
+                    <li className={`flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-black-100 after:border-4 after:inline-block dark:after:border-black-500`}>
+                        <span className={`flex items-center text-white sm:md:text-xs md:text-sm justify-center w-20 h-20 ${obtenerClaseColor(2)} rounded-full md:h-24 md:w-24 sm:w-20 sm:h-20 lg:h-24 lg:w-24 xl:h-28 xl:w-28 dark:bg-gray-700 shrink-0`}>
+                            <p className="text-sm xl:text-base">Rectificando</p>
+                        </span>
+                    </li>
+                    <li className={`flex items-center w-full`}>
+                        <span className={`flex items-center text-white text-center sm:md:text-xs md:text-sm justify-center w-20 h-20 ${obtenerClaseColor(3)} rounded-full md:h-24 md:w-24 sm:w-20 sm:h-20 lg:h-24 lg:w-24 xl:h-28 xl:w-28 dark:bg-gray-700 shrink-0`}>
+                            <p className="text-sm xl:text-base">Aprobado <br />o <br /> Rechazado</p>
+                        </span>
+                    </li>
+                </ol>
+            </div>
+        </>
+    );
+}
+
+export default Iconos;
