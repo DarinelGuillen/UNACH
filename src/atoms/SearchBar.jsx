c4eb3d59e955594db1680998dd10ebc1d76649d7
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2023-08-24 11:11:29 -0600

    search bar fixed

---
+++ src/atoms/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const SearchBar = () => {
+    return (
+        <>
+            {/* shadow-xl */}
+            <div class="relative justify-center">
+                <div class="w-[90%] mx-auto relative bg-white px-6 pt-10 pb-8 ring-gray-900/5 sm:mx-auto md:w-[80%] xl:w-[70%] sm:px-10">
+                    <form action="" class="relative w-full">
+                        <input type="search" class="cursor-pointer relative z-10 h-12 w-12 rounded-full border bg-transparent pl-12 outline-none w-full focus:cursor-text focus:pl-16 focus:pr-4"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-gray-500 px-3.5 border-black border-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
+                    </form>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SearchBar
--- src/pages/RegistroProyectos.jsx
+++ src/pages/RegistroProyectos.jsx
@@ -4,6 +4,7 @@ import CardRight from '../atoms/cardRight';
 import Footer from '../atoms/Footer';
 import CardXl from '../atoms/cardXl';
 import Header2 from '../atoms/header2';
+import SearchBar from '../atoms/SearchBar';
 
 const RegistroProyectos = () => {
     const [cardXlData, setCardXlData] = useState([
@@ -59,10 +60,10 @@ const RegistroProyectos = () => {
             <div className='bg-white font-sans'>
 
                 <Header2 /> 
-
-                <div className="flex justify-center xl:mt-14">
+                {/* <div className="flex justify-center xl:mt-14">
                     <input className='rounded-3xl border-[1px] border-black h-10 w-5/6 md:w-[80%] lg:h-14 lg:mb-5' type="text" />
-                </div>
+                </div> */}
+                <SearchBar />
 
                 <div className="pb-5 mt-5">
                     <div className="grid grid-cols-2 grid-rows-3 gap-10 bg-white xl:grid-cols-3 xl:grid-rows-2 xl:ml-[3%] xl:mt-10 xl:mb-10">
