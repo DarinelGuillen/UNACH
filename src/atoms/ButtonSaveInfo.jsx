a0837d7bd810d6e08a036021e4f8410809847f07
LuisAxelMarínGarcía <213467@ids.upchiapas.edu.mx>
2023-08-25 13:18:18 -0600

    simulacion fetch

--- src/atoms/ButtonSaveInfo.jsx
+++ src/atoms/ButtonSaveInfo.jsx
@@ -1,32 +1,53 @@
-import { useContext, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import SharedDataContext from "../contexts/SharedDataContext";
 import { dataDictionary } from "../webComponents/input";
 import { dataDictionaryTextArea } from "../webComponents/textArea";
-import logoSave from "../assets/img/Icon/Save.svg"
-function ButtonSaveInfo() {
-  const { isShareData, setIsShareData } = useContext(SharedDataContext);
+import logoSave from "../assets/img/Icon/Save.svg";
 
-  const HandlerClickSetData = ((e) => {
-    e.preventDefault();
-    
-    // Merge dataDictionary and dataDictionaryTextArea into a single object
-    const mergedData = { ...dataDictionary, ...dataDictionaryTextArea };
-    
-    setIsShareData(mergedData);
-    console.log("🚀 ~ file: button.jsx:13 ~ HandlerClickSetData ~ isShareData:", isShareData);
-  })
-  
+function ButtonSaveInfo() {
+    const { isShareData, setIsShareData } = useContext(SharedDataContext);
+
+    const handleSave = async () => {
+        const mergedData = { ...dataDictionary, ...dataDictionaryTextArea };
+
+        try {
+            setIsShareData(mergedData);
+
+            const response = await fetch('url_del_servidor', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(mergedData)
+            });
+
+            if (response.ok) {
+                console.log('Datos guardados exitosamente');
+            } else {
+                console.error('Error al guardar los datos');
+            }
+        } catch (error) {
+            console.error('Error en la solicitud:', error);
+        }
+    }
 
     useEffect(() => {
         console.log("🚀 ~ useEffect ~ dataDictionary:", dataDictionary);
     }, [dataDictionary]);
-    return ( 
-        <>
-        <button onClick={HandlerClickSetData} ><img src={logoSave} className='h-[40px] w-[30px] lg:w-[40px] lg:h-[50px] md:h-[30px] md:mr-[10px] sm:h-[30px] xl:mr-[10px]'></img></button>
 
-        {/* <button onClick={HandlerClickSetData}>Set information into ShareDataContext</button> */}
-        </>
-     );
+    useEffect(() => {
+        console.log("🚀 ~ useEffect ~ isShareData:", isShareData);
+    }, [isShareData]);
+
+    return (
+        <button onClick={handleSave}>
+            <img
+                src={logoSave}
+                className='h-[40px] w-[30px] lg:w-[40px] lg:h-[50px] md:h-[30px] md:mr-[10px] sm:h-[30px] xl:mr-[10px]'
+                alt="Guardar"
+            />
+        </button>
+    );
 }
 
 export default ButtonSaveInfo;
